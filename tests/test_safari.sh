f36a105f8b2f34fc19f2046590eb75e08b31297f
David Garske <david@wolfssl.com>
2021-07-19 14:55:34 -0700

    Move the ETSI documentation into `docs/README.md`. Remove copies of specs and use links. Updated Safari test.

--- tests/test_safari.sh
+++ tests/test_safari.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
 # run from wolfKeyMgr root directory
+cp ./tests/test_safari.vault ./tests/test_safari.vault.orig
 ./src/wolfkeymgr -v ./tests/test_safari.vault &
+RESULT=$?
+[ $RESULT -ne 0 ] && echo -e "\nError starting key manager" && exit 1
+
+sleep 1
 ./examples/middlebox/decrypt ./tests/test_safari.pcapng https://localhost:8119
+RESULT=$?
+
+pkill -TERM wolfkeymgr
+sleep 1
+rm -f ./tests/test_safari.vault
+mv ./tests/test_safari.vault.orig ./tests/test_safari.vault
+
+exit 0
